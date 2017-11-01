be2284ba674aac3cbbf95e4b1356e87acce400ee
Joren Vandeweyer <joren@Joren.local>
2017-11-01 03:30:09 +0100

    README and file renaming

--- cah.js
+++ cah.js
@@ -1,9 +1,11 @@
 const cards = require("./content/cards");
 const Player = require("./src/player");
-const round = require("./src/round");
+const Round = require("./src/round");
 
-module.exports = class CAH {
-    constructor(owner, packs="Base", cards=5, rounds=5){
+var game;
+
+class CAH {
+    constructor(owner, packs=["Base"], cards=5, rounds=5){
         if(cards < 3){
             cards = 3;
         }
@@ -16,6 +18,7 @@ module.exports = class CAH {
         this.cards = cards;
         this.rounds = rounds;
 
+        this.round;
         this.currentRound = 0;
 
         this.whiteCards = [];
@@ -29,30 +32,27 @@ module.exports = class CAH {
         return cards.order;
     }
 
-    private get whiteCard(){
+    get whiteCard(){
         if(this.whiteCards.length == 0){
-            this.fillDecks(black=false);
+            this.fillDecks(true, false);
         }
 
-        return this.whiteCards.splice(Math.floor(Math.random()*this.whiteCards.length), 1);
+        return this.whiteCards.splice(Math.floor(Math.random()*this.whiteCards.length), 1)[0];
     }
 
     get blackCard(){
         if(this.blackCards.length == 0){
-            this.fillDecks(white=false);
+            this.fillDecks(false, true);
         }
 
-        return this.blackCards.splice(Math.floor(Math.random()*this.blackCards.length), 1);
-    }
-
-    get round(){
-        return this.round;
+        return this.blackCards.splice(Math.floor(Math.random()*this.blackCards.length), 1)[0];
     }
 
     fillDecks(white=true, black=true){
         for (let i = 0; i<this.packs.length; i++){
             if(cards.order.includes(this.packs[i])){
                 let pack = cards[this.packs[i]];
+                //console.log(pack);
                 if(white){
                     this.whiteCards = this.whiteCards.concat(pack.white);
                 }
@@ -67,22 +67,52 @@ module.exports = class CAH {
         }
     }
 
-    addPlayer(id){
+    join(id){
         let player = new Player(id);
 
-        while(player.getCards.length < this.cards){
-            player.addCard(this.whiteCard);
+        while(player.Cards.length < this.cards){
+            player.addCard(cards.whiteCards[this.whiteCard]);
         }
 
         this.players[id] = player;
+
+        if(this.round){
+            this.round.join(player);
+        }
+
+        return [{
+            action: "joined",
+            id: id,
+            description: "%player has joined the game"
+        }];
+    }
+
+    leave(id){
+        let player = this.players[id];
+        if(player == undefined) return {status: "error", description: "%player is not in the game"};
+
+        delete this.players[id];
+        if(Object.keys(this.player).length < 3){
+            return [{
+                status: "left",
+                id: id,
+                description: "%player left the game, not enough players left, games has ended"
+            }];
+        } else {
+            return [{
+                status: "left",
+                id: id,
+                description: "%player left the game"
+            }];
+        }
     }
 
     start(){
-        if (Object.keys(players).length <= 3){
-            return {
+        if (Object.keys(this.players).length < 3){
+            return [{
                 status: "error",
                 description: "Not enough players yet!"
-            }
+            }];
         }
 
         return this.nextRound();
@@ -95,19 +125,72 @@ module.exports = class CAH {
                 description: "The game is finished, start another one!"
             }
         }
-        let blackPlayer = Obecjt.keys(players)[Math.floor(Math.random()*Object.keys(players).length)];
-        let blackCards [];
+        let blackPlayer = Object.keys(this.players)[Math.floor(Math.random()*Object.keys(this.players).length)];
+        let blackCards = [];
 
         while(blackCards.length < this.cards){
-            blackCards.push(this.blackCard);
+            blackCards.push(cards.blackCards[this.blackCard]);
+
         }
 
-        return {
-            status: "round"
-            blackPlayer: blackPlayer,
-            blackCards: blackCards
+        this.round = new Round(blackPlayer, blackCards, this.players);
+
+        let description_private = "Choose a question:";
+
+        for(let i = 0; i < blackCards.length; i++){
+            description_private += "\n" + i + ": " + blackCards[i].text;
+        }
+        return [{
+            status: "round",
+            private: true,
+            id_private: [blackPlayer],
+            description_private: [description_private],
+            id: blackPlayer,
+            description: "%player has to choose a black card"
+        }];
+    }
+
+    choose(id, cards){
+        if(this.round){
+            let round = this.round.choose(id, cards);
+            if(round[0].status == "roundWon"){
+                return round.concat(this.nextRound());
+            } else {
+                return round;
+            }
+        } else {
+            return [{
+                status: "error",
+                description: "there is no round going on."
+            }];
         }
     }
 
 
+}
+
+module.exports = class CAHGame{
+    constructor(id, packs=["Base"], cards=5, rounds=5){
+        this.cah = new CAH(id, packs=packs, cards=cards, rounds=rounds);
+    }
+
+    join(id){
+        return this.cah.join(id);
+    }
+
+    choose(id, cards){
+        return this.cah.choose(id, cards);
+    }
+
+    start(){
+        return this.cah.start();
+    }
+
+    leave(id){
+        return this.cah.leave(id);
+    }
+
+    addPacks(pack){
+        return this.cah.addPacks(packs);
+    }
 }
